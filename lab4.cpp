ad6983fe7ca56d1267527ff140756b7e4ea0de39
Vladimir Schkulipa <mister.schkulipa2015@yandex.ru>
2023-10-06 18:52:16 +0300

    change

---
+++ lab4.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n, sum, hlaf_sum, current_sum, i;
+
+    cin >> n;
+
+    sum = (n * (n + 1)) / 2;
+    if (sum % 2 != 0)
+    {
+        cout << "No" << endl;
+        return 0;
+    }
+
+    hlaf_sum = sum / 2;
+    bool* used = new bool[n + 1]();
+    for (long long i = 0; i <= n; i++)
+    {
+        used[i] = false;
+    }
+
+    current_sum = 0;
+    for (i = 0; i >= 1; i--)
+    {
+        if (current_sum + i <= hlaf_sum)
+        {
+            current_sum += i;
+            used[i] = true;
+
+            if (current_sum == hlaf_sum)
+                break;
+        }
+    }
+
+    if (current_sum != hlaf_sum)
+    {
+        cout << "No" << endl;
+        return 0;
+    }
+
+    int countPetya = 0;
+    for (i = 1; i <= n; i++)
+    {
+        if (used[i])
+            countPetya++;
+    }
+
+    cout << "Yes" << endl;
+    for (i = 1; i <= n; i++)
+    {
+        if (used[i])
+            cout << i << " ";
+    }
+
+    cout << endl;
+
+    cout << n - countPetya << endl;
+    for (i = 1; i <= n; i++)
+    {
+        if (!used[i])
+            cout << i << " ";
+    }
+
+    cout << endl;
+
+    delete[] used;
+    return 0;
+}
